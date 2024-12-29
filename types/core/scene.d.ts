f7480ba93c495c08b595cf0f6db85cc9dc6cdcf9
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-12-28 23:04:05 -0500

    Minor change

--- types/core/scene.d.ts
+++
@@ -1,8 +0,0 @@
-export declare abstract class Scene {
-    private _initialized;
-    private _initialize;
-    abstract initialize(): void | Promise<void>;
-    abstract start(): void | Promise<void>;
-    abstract stop(): void;
-    abstract update(): void;
-}
