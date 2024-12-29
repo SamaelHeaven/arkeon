0fbf0b5edaef34414e84d8dc44dcb74dbb8fd65e
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-12-29 18:30:53 -0500

    Cleaning build and types folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import { deleteSync } from "del";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
@@ -10,6 +11,12 @@ export default {
         name: "arkeon"
     },
     plugins: [
+        {
+            name: "clean-folders",
+            buildStart() {
+                deleteSync(["build", "types"]);
+            }
+        },
         resolve(),
         commonjs(),
         typescript({
--- types/globals/set.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-    interface Set<T> {
-        addAll(elements: Iterable<T>): void;
-        deleteAll(elements: Iterable<T>): void;
-    }
-}
-export {};
