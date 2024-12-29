c459e014aa50d5b1f399a30bcb05c82594463464
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-12-29 18:44:22 -0500

    Refactor: Extracted time related modules

---
+++ types/time/duration.d.ts
@@ -0,0 +1,23 @@
+import { DurationUnit } from "./duration-unit";
+export declare class Duration {
+    private readonly _nanoseconds;
+    constructor(value: number, unit: DurationUnit);
+    static fromNanoseconds(value: number): Duration;
+    static fromMicroseconds(value: number): Duration;
+    static fromMilliseconds(value: number): Duration;
+    static fromSeconds(value: number): Duration;
+    static fromMinutes(value: number): Duration;
+    static fromHours(value: number): Duration;
+    static fromDays(value: number): Duration;
+    to(unit: DurationUnit): number;
+    get nanoseconds(): number;
+    get microseconds(): number;
+    get milliseconds(): number;
+    get seconds(): number;
+    get minutes(): number;
+    get hours(): number;
+    get days(): number;
+    add(other: Duration): Duration;
+    subtract(other: Duration): Duration;
+    toString(): string;
+}
