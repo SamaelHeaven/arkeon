bfefe41dea95dc47e89f0d12b5a5b44594c869b8
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-01 19:16:01 -0500

    Feat: Made texture able to be cropped

--- src/rendering/texture.ts
+++ src/rendering/texture.ts
@@ -5,21 +5,35 @@ export type NativeTexture = HTMLImageElement | HTMLCanvasElement | OffscreenCanv
 
 export class Texture {
     private readonly _nativeTexture: NativeTexture;
+    public readonly position: Vec2;
+    public readonly size: Vec2;
 
-    constructor(nativeTexture: NativeTexture) {
+    constructor(nativeTexture: NativeTexture, position?: Vec2, size?: Vec2) {
         this._nativeTexture = nativeTexture;
+        const textureSize = new Vec2(this._nativeTexture.width, this._nativeTexture.height);
+        this.position = position ? position.clamp(0, textureSize) : Vec2.ZERO;
+        const maxSize = textureSize.minus(this.position);
+        this.size = size ? size.clamp(0, maxSize) : maxSize;
+    }
+
+    public get x(): number {
+        return this.position.x;
+    }
+
+    public get y(): number {
+        return this.position.y;
     }
 
     public get width(): number {
-        return this._nativeTexture.width;
+        return this.size.x;
     }
 
     public get height(): number {
-        return this._nativeTexture.height;
+        return this.size.y;
     }
 
-    public get size(): Vec2 {
-        return new Vec2(this.width, this.height);
+    public crop(position: Vec2, size: Vec2) {
+        return new Texture(this._nativeTexture, position, size);
     }
 }
 
--- types/rendering/texture.d.ts
+++ types/rendering/texture.d.ts
@@ -3,10 +3,14 @@ import { Graphics } from "./graphics";
 export type NativeTexture = HTMLImageElement | HTMLCanvasElement | OffscreenCanvas;
 export declare class Texture {
     private readonly _nativeTexture;
-    constructor(nativeTexture: NativeTexture);
+    readonly position: Vec2;
+    readonly size: Vec2;
+    constructor(nativeTexture: NativeTexture, position?: Vec2, size?: Vec2);
+    get x(): number;
+    get y(): number;
     get width(): number;
     get height(): number;
-    get size(): Vec2;
+    crop(position: Vec2, size: Vec2): Texture;
 }
 export declare class WritableTexture extends Texture {
     readonly graphics: Graphics;
